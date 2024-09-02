9a05490ce91add8a024566f30cef06e23466b661
Cam <chrlew082@gmail.com>
2024-09-02 12:25:27 -0600

    Automatically get IP Address, and fix arguments

--- src/main/java/dev/expx/payments/stores/tebex/command/sub/BanSub.java
+++ src/main/java/dev/expx/payments/stores/tebex/command/sub/BanSub.java
@@ -8,10 +8,12 @@ import net.minestom.server.command.builder.Command;
 import net.minestom.server.command.builder.arguments.Argument;
 import net.minestom.server.command.builder.arguments.ArgumentString;
 import net.minestom.server.command.builder.arguments.ArgumentType;
+import net.minestom.server.command.builder.exception.ArgumentSyntaxException;
 import net.minestom.server.command.builder.suggestion.SuggestionEntry;
 import net.minestom.server.entity.Player;
 import org.slf4j.LoggerFactory;
 
+import java.net.InetSocketAddress;
 import java.util.concurrent.ExecutionException;
 
 public class BanSub extends Command {
@@ -26,14 +28,14 @@ public class BanSub extends Command {
         });
 
         Argument<String> reason = ArgumentType.String("reason").setDefaultValue("None Specified");
-        Argument<String> ip = ArgumentType.String("ip").setDefaultValue("");
 
-        addConditionalSyntax((s, str) -> {
-            return s.hasPermission("minestom.store.ban");
-        }, (s, c) -> {
+        addConditionalSyntax((s, str) -> s.hasPermission("minestom.store.ban"), (s, c) -> {
             if(!handler.isSetup()) throw new RuntimeException("Plugin not setup.");
             try {
-                boolean success = handler.getSDK().createBan(c.get(playerArg), c.get(ip), c.get(reason)).get();
+                Player player = MinecraftServer.getConnectionManager().getOnlinePlayerByUsername(c.get(playerArg));
+                if(player == null) throw new ArgumentSyntaxException("Invalid Player", c.get(playerArg), 0);
+                InetSocketAddress address = (InetSocketAddress) player.getPlayerConnection().getRemoteAddress();
+                boolean success = handler.getSDK().createBan(c.get(playerArg), address.toString(), c.get(reason)).get();
                 if(success)
                     s.sendMessage(Component.text(" Player banned from WebStore successfully.", NamedTextColor.GREEN));
                 else
@@ -42,7 +44,7 @@ public class BanSub extends Command {
                 s.sendMessage("An internal error has occurred. Please see console for more information.");
                 LoggerFactory.getLogger(BanSub.class).error("An error has occurred while trying to ban a player from the store: " + ex.getMessage());
             }
-        });
+        }, playerArg, reason);
 
     }
 
